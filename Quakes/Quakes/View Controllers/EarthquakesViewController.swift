81c507d4f0678b143937a56f24d563fe490067bf
David Wright <davidtwright89@gmail.com>
2020-05-13 23:22:59 -0400

    Added a detail popup to each annotation, and a custom glyph image

--- Quakes/Quakes/View Controllers/EarthquakesViewController.swift
+++ Quakes/Quakes/View Controllers/EarthquakesViewController.swift
@@ -84,6 +84,13 @@ extension EarthquakesViewController: MKMapViewDelegate {
             preconditionFailure("Missing the registered map annotation view")
         }
         
+        annotationView.glyphImage = #imageLiteral(resourceName: "QuakeIcon")
+        
+        annotationView.canShowCallout = true
+        let detailView = QuakeDetailView()
+        detailView.quake = quake
+        annotationView.detailCalloutAccessoryView = detailView
+        
         return annotationView
     }
 }
